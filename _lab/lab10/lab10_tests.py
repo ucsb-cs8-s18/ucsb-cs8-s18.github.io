09dc2c3288839f4ccc1bd2bba16a34a8df201cc5
Phill Conrad <pconrad@cs.ucsb.edu>
2018-06-03 21:18:44 -0700

    update lab10_tests

--- _lab/lab10/lab10_tests.py
+++ _lab/lab10/lab10_tests.py
@@ -4,9 +4,13 @@ import pytest
 
 from lab10 import spacedString
 
+def test_spacedString_0():
+    assert spacedString("",1)==""
+
 def test_spacedString_1():
     assert spacedString("UCSB",1)=="U C S B"
 
+    
 def test_spacedString_2():
     assert spacedString("Cal Poly",2)=="C  a  l     P  o  l  y"
 
@@ -40,5 +44,31 @@ def test_spacedString_10():
         
 ##########################
 
+from lab10 import indicesOfEvens
 
+def test_indicesOfEvens_0():
+    assert indicesOfEvens([])==[]
+
+def test_indicesOfEvens_1():
+    assert indicesOfEvens([11,20,35,44,57,63,42])==[1,3,6]
+
+def test_indicesOfEvens_2():
+    assert indicesOfEvens([3,5,7,9])==[]    
+
+def test_indicesOfEvens_3():
+    assert indicesOfEvens([3,4,5])==[1]
+
+def test_indicesOfEvens_4():
+    assert indicesOfEvens([3,4,6])==[1,2]        
+    
+def test_indicesOfEvens_5():
+    assert indicesOfEvens([4,3,4,6])==[0,2,3]        
+    
+def test_indicesOfEvens_6():
+    with pytest.raises(ValueError):
+      result = indicesOfEvens("foo")
+
+def test_indicesOfEvens_7():
+    with pytest.raises(ValueError):
+      result = indicesOfEvens([4,5,"bar"])
 
