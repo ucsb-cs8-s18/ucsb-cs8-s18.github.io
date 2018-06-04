0b1090138013850cbe5de7396447f90557636cc7
Phill Conrad <pconrad@cs.ucsb.edu>
2018-06-03 22:23:46 -0700

    lab10 starter code

--- _lab/lab10/lab10_tests.py
+++ _lab/lab10/lab10_tests.py
@@ -1,6 +1,37 @@
 import pytest
 
+##############
+
+from lab10 import midi2freq
+
+
+def test_midi2freq_21():
+    assert midi2freq(21)==pytest.approx(27.50,abs=1e-2)
+
+def test_midi2freq_33():
+    assert midi2freq(33)==pytest.approx(55.00,abs=1e-2)
+
+def test_midi2freq_60():
+    assert midi2freq(60)==pytest.approx(261.63,abs=1e-2)
+
+def test_midi2freq_69():
+    assert midi2freq(69)==pytest.approx(440.000,abs=1e-2)
+
+def test_midi2freq_foo():
+    with pytest.raises(ValueError):
+       result = midi2freq("foo")
+
+def test_midi2freq_n1():
+    with pytest.raises(ValueError):
+       result = midi2freq(-1)
+
+def test_midi2freq_128():
+    with pytest.raises(ValueError):
+       result = midi2freq(128)
+
+       
 #############
+
 
 from lab10 import spacedString
 
@@ -72,3 +103,37 @@ def test_indicesOfEvens_7():
     with pytest.raises(ValueError):
       result = indicesOfEvens([4,5,"bar"])
 
+###################################
+
+from lab10 import countInRange
+
+def test_countInRange_1():
+    assert countInRange([10,20,30,40],20,30)==1
+
+def test_countInRange_2():
+    assert countInRange([10,20,30,40],20,40)==2
+
+def test_countInRange_3():
+    assert countInRange([10,20,30,40],10,41)==4
+
+def test_countInRange_4():
+    assert countInRange([10,20,30,40],0,10)==0    
+
+def test_countInRange_5():
+    assert countInRange([75,84,92,76,74,97,80,100,54],70,80)==3
+
+def test_countInRange_6():
+    with pytest.raises(ValueError):
+      result = countInRange([4,5,"bar"],30,40)
+
+def test_countInRange_7():
+    with pytest.raises(ValueError):
+      result = countInRange([4,5,6],"foo",40)
+
+def test_countInRange_8():
+    with pytest.raises(ValueError):
+      result = countInRange([4,5,6],40,"bar")
+    
+def test_countInRange_9():
+    with pytest.raises(ValueError):
+      result = countInRange("foo",40,50)
