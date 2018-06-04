57e05220dfdb8b1526d1312c21fb67d50c371777
Phill Conrad <pconrad@cs.ucsb.edu>
2018-06-03 20:51:53 -0700

    Create lab10_tests.py

---
+++ _lab/lab10/lab10_tests.py
@@ -0,0 +1,31 @@
+import pytest
+
+#############
+
+from lab10 import recursiveDigitSum
+
+def test_spacedString_1():
+    assert spacedString("UCSB",1)=="U C S B"
+
+def test_spacedString_2():
+    assert spacedString("Cal Poly",2)=="C  a  l     P  o  l  y"
+
+def test_spacedString_3():
+    assert spacedString("UCSD",0)=="UCSD"
+
+def test_spacedString_4():
+   with pytest.raises(ValueError):
+      result = spacedString("UCSD",-1)
+
+def test_spacedString_5():
+   with pytest.raises(ValueError):
+      result = spacedString(["not a string"],0)
+
+def test_spacedString_5():
+   with pytest.raises(ValueError):
+      result = spacedString("A String","Not an int")
+      
+##########################
+
+
+
