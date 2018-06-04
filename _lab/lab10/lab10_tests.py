57d0afd45d3221a493679f4615eb6b4b86601c59
Phill Conrad <pconrad@cs.ucsb.edu>
2018-06-03 20:56:53 -0700

    Update lab10_tests.py

--- _lab/lab10/lab10_tests.py
+++ _lab/lab10/lab10_tests.py
@@ -21,10 +21,23 @@ def test_spacedString_5():
    with pytest.raises(ValueError):
       result = spacedString(["not a string"],0)
 
-def test_spacedString_5():
+def test_spacedString_6():
    with pytest.raises(ValueError):
       result = spacedString("A String","Not an int")
-      
+
+def test_spacedString_7():
+    assert spacedString("U",1)=="U"
+
+def test_spacedString_8():
+    assert spacedString("UC",1)=="U C"
+
+def test_spacedString_9():
+    assert spacedString(" A ",1)=="  A  "
+
+def test_spacedString_10():
+    assert spacedString("X X",1)=="X   X"
+
+        
 ##########################
 
 
